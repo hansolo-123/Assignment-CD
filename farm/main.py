2a4724359a69bfd53005354059f0c89b1738a58c
Hans Hoefman <hoefman.hans@gmail.com>
2023-05-12 22:08:31 +0200

    test code pytest_4

--- farm/main.py
+++ farm/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, yoxcvsczxuu!'
+    return 'Hello, yoxcvsczxuuuu!'
 
 @app.route('/cow')
 def cow():
